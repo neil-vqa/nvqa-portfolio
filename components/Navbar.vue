e9381c6c324344c92b637c0d7fb14699c4103931
Neil Alino <nvq.alino@gmail.com>
2020-11-22 11:29:25 +0800

    update transition speed and target blank for blog link

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -30,7 +30,7 @@
 				<h3 class="text-xs sm:text-base">Contact</h3>
 			</div>
 		</NuxtLink>
-		<a href="https://blog.neilvqa.host/">
+		<a href="https://blog.neilvqa.host/" target="_blank">
 			<div class="flex flex-col justify-center items-center mx-2 sm:mx-4">
 				<svg class="w-6 h-6 sm:w-10 sm:h-10" fill="none" stroke="#2b6cb0" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"></path></svg>
 				<h3 class="text-xs sm:text-base">Blog↗</h3>
